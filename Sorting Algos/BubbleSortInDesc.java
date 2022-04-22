b65d8a65a6b2c46e55bac43b747c20a15465efc8
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-04-22 20:35:11 +0530

    Bubble Sort in descending order

---
+++ Sorting Algos/BubbleSortInDesc.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class BubbleSortInDesc {
+    public static void main(String[] args) {
+       int [] arrayToSort = {7,0,-9,-4,7,3,2,8};
+       bubbleSort(arrayToSort);
+       System.out.println(Arrays.toString(arrayToSort));
+    }
+    
+    static void bubbleSort(int [] arrayToSort){
+        int sizeOfInputArr = arrayToSort.length;
+        
+        for (int i = 0; i < sizeOfInputArr -1 ; i++){
+            for(int j = 0; j < sizeOfInputArr - i -1 ; j ++){
+                if(arrayToSort[j] < arrayToSort[j+1]){
+                    int temp = arrayToSort[j];
+                    arrayToSort[j] =arrayToSort[j+1];
+                    arrayToSort[j+1]= temp;
+                }
+            }
+        }
+    }
+}
