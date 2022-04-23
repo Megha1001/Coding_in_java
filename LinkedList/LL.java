97e22d9121cb7bfc3053bed2678b38073fe40860
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-04-23 21:39:26 +0530

    LinkedList implementation

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -1,5 +1,11 @@
 class LL {
     Node head;
+    private int size;
+    
+    LL(){
+        this.size = 0;
+    }
+    
     class Node{
         String data;
         Node next;
@@ -7,6 +13,7 @@ class LL {
         Node(String data){
             this.data = data;
             this.next = null;
+            ++size;
         }
     }
     
@@ -54,19 +61,67 @@ class LL {
             currNode = currNode.next;
         }
         
-        System.out.print("NULL");
+        System.out.println("NULL");
         
     }
     
+    
+    // delete first element
+    public void deleteFirst(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        
+        head = head.next;
+        --size;
+    }
+    
+    // delete last element
+    public void deleteLast(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        
+        --size;
+        if(head.next == null){
+            head = null;
+            return;
+        }
+        
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+    }
+    
+    // to get size
+    public int getSize(){
+        return size;
+    }
     
     public static void main(String [] args){
         LL list = new LL();
         
         list.addAtFirst("a");
         list.addAtFirst("b");
+        
         list.addAtLast("c");
+        list.addAtLast("d");
+        list.addAtLast("e");
         
         list.printList();
+        
+        list.deleteFirst();
+        list.printList();
+        
+        list.deleteLast();
+        list.printList();
+        System.out.println(list.getSize());
         
     }
 }
