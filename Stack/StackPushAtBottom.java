897dbae9d97cba90e8cf2642bcdc2d51c09890b0
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-04-25 18:14:37 +0530

    Insert element at bottom of Stack

---
+++ Stack/StackPushAtBottom.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class StackPushAtBottom {
+    
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        
+        pushAtBottom(s, 5);
+        
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
