cdeebb86640235b3d904ede884b3e5445292cb32
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-04-25 18:44:25 +0530

    Reverse the Stack using Recursion(In-place) approach

---
+++ Stack/StackReverseThRecursion.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+/*Technique : Using recursion remove top element from Stack , once it gets empty, return the stack then push popped element at the bottom of the Stack.
+*/
+
+class StackReverseThRecursion{
+    
+    public static void pushAtBottom (Stack<Integer> s, int data){
+        
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    
+    public static void reverse(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
+    }
+    
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        
+        reverse(stack);
+        
+        while(!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
