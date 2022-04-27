2a1b396e6abad8180643c6147a91f85d0b126cfd
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-04-27 22:30:18 +0530

    PreOrder, InOrder & PostOrder Traversal

--- Trees/BinaryTrees/BinaryTreePreOrder.java
+++ Trees/BinaryTrees/BinaryTreePreOrder.java
@@ -1,4 +1,4 @@
-class BinaryTreePreOrder {
+class BinaryTreeTraversal {
     
     static class Node{
         int data;
@@ -37,17 +37,46 @@ class BinaryTreePreOrder {
            preOrder(root.left);
            preOrder(root.right);
        }
+       
+       public static void inOrder(Node root){
+           if(root == null){
+               return;
+           }
+           
+           inOrder(root.left);
+           System.out.println(root.data);
+           inOrder(root.right);
+       }
+       
+       public static void postOrder(Node root){
+           if(root == null){
+               return;
+           }
+           
+           postOrder(root.left);
+           postOrder(root.right);
+           System.out.println(root.data);
+       }
     }
     
     
     public static void main(String[] args) {
-        //Pre-order traversal - NLR(Node, Left, Right)
         int nodes[] = {1, 2, 4, -1, -1, 5, -1,-1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println("Node of tree : "+ root.data);
+        
+        //Pre-order traversal - NLR (Node, Left, Right)
         System.out.println("PREORDER TRAVERSAL");
         tree.preOrder(root);
+        
+        //In-order traversal - LNR (Left, Node, Right)
+        System.out.println("INORDER TRAVERSAL");
+        tree.inOrder(root);
+        
+        //Post-order traversal - LRN (Left, Node, Right)
+        System.out.println("POSTORDER TRAVERSAL");
+        tree.postOrder(root);
         
     }
     
@@ -60,6 +89,21 @@ class BinaryTreePreOrder {
     4
     5
     3
-    6*/
+    6
+    INORDER TRAVERSAL
+    4
+    2
+    5
+    1
+    3
+    6
+    POSTORDER TRAVERSAL
+    4
+    5
+    2
+    6
+    3
+    1
+    */
     
 }
