5047460989874eb8cf2acaa07696212d3b9eaa6d
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-04-30 16:33:13 +0530

    Move all 'x' to the end of String AP2

---
+++ Recursion/MediumLevelProb/MoveElementToEndAP2.java
@@ -0,0 +1,27 @@
+public class MoveElementToEndAP2 {
+	public static String resultString = "";
+	public static int ctr = 0;
+	public static void main(String[] args) {
+		String input = "axbcxxd";
+		char element = 'x';
+		moveElementToEnd(input, 0,'x');
+		for(int i = 0; i < ctr ;i++) {
+			resultString += element;
+		}
+		System.out.println("Input string : "+ input);
+		System.out.println("Resultant string : "+ resultString);
+	}
+	
+	public static void moveElementToEnd(String input, int currIdx, char element) {
+		if(currIdx == input.length()) {
+			return;
+		}
+		if(input.charAt(currIdx) != element) {
+			resultString += input.charAt(currIdx);
+		}else {
+			++ctr;
+		}
+		moveElementToEnd(input,currIdx + 1, element);
+	}
+
+}
