ba51cac8d1ca737315a4967ee20be520ddedf6b6
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-05-01 12:56:35 +0530

    Maximum subarray sum by Kadane's algo [O(N)]

---
+++ DSA Practice Problems/Array/MaxSubArraySumKadaneAlgo.java
@@ -0,0 +1,25 @@
+//Time Complexity - O(N)
+public class MaxSubArraySumKadaneAlgo {
+	public static int maximumSubArraySum(int [] input) {
+		int maxSum = Integer.MIN_VALUE;
+		int currSum = 0;
+		
+		for(int i =0 ; i < input.length ; i++) {
+			currSum += input[i];
+			if(currSum > maxSum) {
+				maxSum = currSum;
+			}
+			else if(currSum < 0) {
+				currSum = 0;
+			}
+		}
+		return maxSum;
+	}
+	
+	public static void main(String[] args) {
+		int [] input = {1, 3, 8, -2, 6, -8, 5};
+		System.out.println("Maximum sub of subarray : "+maximumSubArraySum(input));
+
+	}
+
+}
