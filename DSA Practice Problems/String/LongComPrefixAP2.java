f0bcc08bd5cf4bb5cabcf0e35f56b36f6c37d692
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-05-03 13:13:56 +0530

    Find longest common Prefix approach 2

---
+++ DSA Practice Problems/String/LongComPrefixAP2.java
@@ -0,0 +1,20 @@
+public class LongComPrefixAP2 {
+	public static String longestCommonPrefix(String [] strs) {
+		if(strs == null || strs.length ==0) return "";
+		String pre = strs[0];
+		int i = 1;
+		while(i < strs.length) {
+			while(strs[i].indexOf(pre) != 0) {
+				pre = pre.substring(0, pre.length() -1);
+			}
+		i++;
+		}
+		
+		return pre;
+	}
+	public static void main(String[] args) {
+		String [] input = {"flower","flow","flight"};
+		System.out.println(longestCommonPrefix(input));
+	}
+
+}
