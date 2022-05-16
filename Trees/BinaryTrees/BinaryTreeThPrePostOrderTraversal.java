c6fe032a417bb39c5e1b73408246ba2c475cdf1f
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-05-16 22:57:16 +0530

    Build Binary Tree from given inorder and postOrder traversal

---
+++ Trees/BinaryTrees/BinaryTreeThPrePostOrderTraversal.java
@@ -0,0 +1,49 @@
+
+public class BinaryTreeThPrePostOrderTraversal {
+
+	public static int preIndex = 0;
+	
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data){
+			this.data = data;
+			this.left = this.right = null;
+		}
+	}
+	
+	public static Node buildTree(int[] in, int [] pre, int start, int end) {
+		
+		// For leaf node child
+		if(start > end) {
+			return null;
+		}
+		
+		// Pre-Order traversal
+		Node root = new Node(pre[preIndex++]);
+		int index = -1;
+		for(int i = start ; i <= end; i++) {
+			if(in[i] == root.data) {
+				index = i;
+				break;
+			}
+		}
+		
+		root.left = buildTree(in, pre, start, index - 1);
+		root.right = buildTree(in, pre, index + 1, end);
+		
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		int [] inOrder = {20,10,40,30,50};
+		int [] preOrder = {10,20,30,40,50};
+		
+		Node root = buildTree(inOrder, preOrder, 0, inOrder.length -1);
+		System.out.println(root.data);
+
+	}
+
+}
