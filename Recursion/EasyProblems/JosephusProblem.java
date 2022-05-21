1177b4996522239016d318b83f822794abc250b3
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-05-21 19:29:32 +0530

    Update JosephusProblem.java

--- Recursion/EasyProblems/JosephusProblem.java
+++ Recursion/EasyProblems/JosephusProblem.java
@@ -16,3 +16,33 @@ public class JosephusProblem {
 	}
 
 }
+
+
+/************
+class Solution {
+    public static int find(int n, int k){
+         if(n == 1){
+            return 0;
+        }
+        
+        return  (find(n-1,k) +k) %n;
+    }
+    
+    public int findTheWinner(int n, int k) {
+        
+       int result = find(n,k);
+        System.out.println(result);
+        return result + 1;
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+*******************/
