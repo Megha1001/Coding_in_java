1684d53e3bf21977cf68bf59d51f6618b4f19fcb
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-05-23 23:03:55 +0530

    Create SecondLargest.java

---
+++ Array/SecondLargest.java
@@ -0,0 +1,33 @@
+package array;
+
+public class SecondLargest {
+
+	public static int getLargest(int[] arr, int length) {
+		int max = arr[0];
+		for(int i = 1; i <= length ; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+	
+	public static int findSecondLarget(int[] arr, int length) {
+		int largest = getLargest(arr, length);
+		int secondLargest = -1;
+		int idxSecondLargest = -1;
+		for(int i = 0; i <= length ; i++) {
+			if(secondLargest < arr[i] && arr[i] != largest) {
+				secondLargest = arr[i];
+				idxSecondLargest = i;
+			}
+		}
+		return idxSecondLargest;
+	}
+	
+	public static void main(String[] args) {
+		int [] input = {1,5,5,5,5};
+		System.out.println(findSecondLarget(input, input.length - 1));
+	}
+
+}
