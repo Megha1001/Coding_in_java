abbf745e55dc75c7c625f7e6942d0bb1c6493955
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-05-26 20:14:11 +0530

    Time complexity : O(N^2)

---
+++ Array/Maximum_difference/MaximumDiff_AP1.java
@@ -0,0 +1,26 @@
+package array;
+
+public class MaximumDiff_AP1 {
+
+  //Time complexity : O(N^2)
+	public static int getMaximumDiff(int [] arr, int n) {
+		int max = Integer.MIN_VALUE;
+		for(int i  = 0; i < n; i++) {
+			for(int j = i+1; j < n; j++) {
+				max = Math.max(arr[j] -arr[i], max);
+			}
+		}
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		int [] arr = {2,3,10,6,4,8,1};
+		System.out.println("Given Array : ");
+		for(int i = 0; i< arr.length ; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("\nMaximum difference is : "+getMaximumDiff(arr,arr.length));
+	}
+
+}
