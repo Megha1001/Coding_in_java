be3b762828f9e35e2d718ea362e2373429a71bc8
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-06-09 21:43:09 +0530

    Create ArraysSortMethod.java

---
+++ Sorting/Arrays/ArraysSortMethod.java
@@ -0,0 +1,19 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class ArraysSortMethod {
+
+	public static void main(String[] args) {
+		int[] arr = {5,20,12,30};
+		char[] arr2 = {'B','B','A','C','A'};
+		
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		Arrays.sort(arr2);
+		System.out.println(Arrays.toString(arr2));
+
+	}
+
+}
