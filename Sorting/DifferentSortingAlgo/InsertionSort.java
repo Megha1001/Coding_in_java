e599ed4bc96563ff76ce34f977969a321f4c92e9
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-06-11 18:06:11 +0530

    Time Complexity - O(N^2)

---
+++ Sorting/DifferentSortingAlgo/InsertionSort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+public class InsertionSortEfficient {
+	
+  //Time Complexity - O(N^2)
+	public static void insertionSort(int[]arr, int n) {
+		for(int i = 1; i < n; i++) {
+			int key = arr[i];
+			int j = i-1;
+			// check for greater element from Right to Left
+			while(j >= 0 && arr[j] > key) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = key;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {5,20,40,60,10,30};
+		insertionSort(arr, arr.length);
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
