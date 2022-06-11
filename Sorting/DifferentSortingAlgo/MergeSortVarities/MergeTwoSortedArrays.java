a6f2598ed1754f6b510423ad67101e3e36600c25
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-06-11 20:15:02 +0530

    Time Complexity : O(m+n)

---
+++ Sorting/DifferentSortingAlgo/MergeSortVarities/MergeTwoSortedArrays.java
@@ -0,0 +1,46 @@
+package sorting;
+
+public class MergeTwoSortedArrays {
+
+	//Time Complexity : O(m+n)
+	public static int[] mergeSort(int[] arr1, int[] arr2) {
+		int[] mergedArr = new int[arr1.length + arr2.length];
+		
+		//idx to monitor first array
+		int idx1 = 0;
+		
+		//idx to monitor second array
+		int idx2 = 0;
+		
+		//idx to monitor merged array
+		int x = 0;
+		
+		while(idx1 < arr1.length && idx2 < arr2.length) {
+			if(arr1[idx1] < arr2[idx2]) {
+				mergedArr[x++] = arr1[idx1++];
+			}else {
+				mergedArr[x++] = arr2[idx2++];
+			}
+		}
+		
+		while(idx1 < arr1.length) {
+			mergedArr[x++] = arr1[idx1++];
+		}
+		
+		while(idx2 < arr2.length) {
+			mergedArr[x++] = arr2[idx2++];
+		}
+		
+		return mergedArr;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr1 = {10,15,20};
+		int[] arr2 = {5,6,6,15};
+		int[] result = mergeSort(arr1,arr2);
+		for(int i = 0; i< result.length; i++) {
+			System.out.print(result[i]+" ");
+		}
+	}
+
+}
