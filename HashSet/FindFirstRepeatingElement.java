308fa72bc98b565de770019563a35927fa8706ca
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-06-15 18:56:41 +0530

    Create FindFirstRepeatingElement.java

---
+++ HashSet/FindFirstRepeatingElement.java
@@ -0,0 +1,32 @@
+package hashset;
+import java.util.HashSet;
+
+public class FindFirstRepeatingElement {
+
+	public static void printFirstRepeating(int[]arr) {
+		int min = -1;
+		
+		HashSet<Integer> set = new HashSet<>();
+		
+		for(int i  = arr.length -1; i >= 0; i--) {
+			if(set.contains(arr[i])) {
+				min = i;
+			}else {
+				set.add(arr[i]);
+			}
+		}
+		
+		if(min != -1) {
+			System.out.println("The first repeating element is :  "+arr[min]);
+		}else {
+			System.out.println("There are no repeating elements.");
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = { 10, 5, 3, 4, 3, 5, 6 };
+		printFirstRepeating(arr);
+	}
+
+}
