86cdfad404a3f6eea4f68598e8e6d480587e932b
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-09-10 16:30:22 +0530

    Using Bitwise & and right shift operator

---
+++ GFG Course Questions/Bit Magic/CountsOneInBinaryReperesentation/CountSetsApp2.java
@@ -0,0 +1,26 @@
+//Time complexity : O(No of bits)
+public class CountSetsApp2{
+    
+    public static void main(String [] args){
+        int n = 5;
+        System.out.println("No of bits with Value 1 in "+n+" is :"+countNoOfBits(n));
+    }
+    
+    static int countNoOfBits(int n){
+        int res = 0;
+        while(n >0){
+            if((n & 1) == 1){
+                ++res;
+            }
+            n = n >>1;
+        }
+        return res;
+    }
+    
+    
+}
+
+/*
+OUTPUT :
+No of bits with Value 1 in 5 is :2s
+*/
