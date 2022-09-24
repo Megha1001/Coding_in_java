076772868e6d0957bd586311db506e0f6c9998b8
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-09-24 15:10:00 +0530

    Create MergeSortedArraysNaiveSolution.java.java

---
+++ GFG Course Questions/Sorting/MergeSort/MergeTwoSortedArrays/MergeSortedArraysNaiveSolution.java.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class MergeSortedArraysNaiveSolution{
+
+    public static void main(String []args){
+        int [] arr1 = {1};
+        int [] arr2 = {1,2,3};
+        mergeSortedArray(arr1, arr2, arr1.length, arr2.length);
+      
+    }
+    
+    static void mergeSortedArray(int [] arr1, int [] arr2, int m, int n){
+        int [] res = new int [m+n];
+        
+        //copy 1st array into result array
+        for(int i = 0; i < m; i++){
+            res[i] = arr1[i];
+        }
+        
+        //copy 2nd array into result array
+        for(int i = 0; i < n; i++){
+            res[i+m] = arr2[i];
+        }
+        
+        //sort the array
+        Arrays.sort(res);
+        
+        System.out.println(Arrays.toString(res));
+        
+    }
+}
