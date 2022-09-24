098443d7d9c23af981863fb18f0a6a52b42af7d1
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-09-24 11:58:01 +0530

    Create BubbleSort.java

---
+++ GFG Course Questions/Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort{
+
+     public static void main(String []args){
+        int [] arr = {10,8,20,5};
+        bubbleSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+     }
+     
+     static void bubbleSort(int [] arr, int n){
+         
+         //runs for n-1 passes
+         for(int i = 0; i < n-1; i++){
+             //after every pass largest element will come at its original position
+             for(int j  = 0 ; j < n-1-i ; j++){
+                 if(arr[j] > arr[j+1]){
+                     //swap using XOR
+                    arr[j] ^= arr[j+1];
+                    arr[j+1] ^= arr[j];
+                    arr[j] ^= arr[j+1];
+                 }
+             }
+         }
+         
+     }
+}
